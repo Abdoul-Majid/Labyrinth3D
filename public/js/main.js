79eb45d1fbd78ea9ff631ca14e6fce58a10dc147
Abdoul-Majid <abdoulmajidtoudjani@gmail.com>
2026-02-01 22:43:44 +0100

    setting up the the threejs scene , renderer and camera

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,31 @@
-alert("hola")
+import * as THREE from 'three';
+
+var scene, camera, renderer;
+var width = 740;
+var height = 500;
+
+const initScene = function() {
+    scene = new THREE.Scene();
+
+    camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
+    camera.position.z = 5;
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(width, height);
+
+    const container = document.getElementById("play-zone");
+    if (container) {
+        container.appendChild(renderer.domElement);
+    }
+
+    function animate() {
+        requestAnimationFrame(animate);
+        renderer.render(scene, camera);
+    }
+    animate();
+}
+
+const setupEvents = function() {
+    initScene();
+}
+
+window.addEventListener("load", setupEvents);
